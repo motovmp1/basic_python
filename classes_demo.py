5dbebb470269d1d0717c0f0b31007c87c73bbd99
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-02 20:06:48 +0800

    running test 5

---
+++ classes_demo.py
@@ -0,0 +1,12 @@
+class MyFirstClass:
+    def m1(self):
+        print("I am here inside the class")
+
+    def __init__(self):
+        print("--->> Here is a constructor")
+
+
+cl = MyFirstClass()
+
+cl.m1()
+
